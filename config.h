fc54228c6efaf5e4579b3fc908db3ce977ba185e
Gary Marigliano <gmarigliano93@gmail.com>
2019-06-19 21:24:46 +0200

    Increase border size

--- config.h
+++ config.h
@@ -9,6 +9,7 @@ static char *font =
     "Sauce Code Pro Nerd Font:pixelsize=19:antialias=true:autohint=true";
 static char *font2[] = {
     "Inconsolata for Powerline:pixelsize=16:antialias=true:autohint=true"};
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
